ebf4cabcd09b4cd2cd22d989ffd1aa9ecdf0e5db
Paavani99 <65934688+Paavani99@users.noreply.github.com>
2023-05-11 23:10:06 +0530

    Update Auth.java
    Removed key from auth.java

--- src/main/java/com/bot/Gpt/Auth.java
+++ src/main/java/com/bot/Gpt/Auth.java
@@ -1,7 +1,7 @@
 package com.bot.gpt;
 
 public class Auth {
-	private static final String bearerToken = "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";
+	private static final String bearerToken = "";
 	
 	public static String getBearertoken() {
 		return bearerToken;
